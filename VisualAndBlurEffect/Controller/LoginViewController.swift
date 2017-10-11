775098bc89207632138860fed8c3dc9543a1a652
devxris <chuwei0915@gmail.com>
2017-10-11 22:32:55 +0800

    Simple UI and setup

---
+++ VisualAndBlurEffect/Controller/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  VisualAndBlurEffect
+//
+//  Created by Chris Huang on 11/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+
+}
+
