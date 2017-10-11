33f7f9c3e5b3b66b5b504fb89e0584672cd82375
devxris <chuwei0915@gmail.com>
2017-10-11 22:51:25 +0800

    implement UIVisualEffectView with UIBlurEffect

--- VisualAndBlurEffect/Controller/LoginViewController.swift
+++ VisualAndBlurEffect/Controller/LoginViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-
+	
+	@IBOutlet weak var backgroundImageView: UIImageView!
+	@IBOutlet weak var labelBlurView: UIVisualEffectView!
+	
+	private let imageSet = ["cloud", "coffee", "food", "pmq", "temple"]
+	
+	private var blurEffectView: UIVisualEffectView?
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		let selectedImageIndex = Int(arc4random_uniform(UInt32(imageSet.count)))
+		backgroundImageView.image = UIImage(named: imageSet[selectedImageIndex])
+		
+		let blurEffect = UIBlurEffect(style: .light)
+		blurEffectView = UIVisualEffectView(effect: blurEffect)
+		blurEffectView?.frame = view.bounds
+		backgroundImageView.addSubview(blurEffectView!)
+	}
+	
+	override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+		blurEffectView?.frame = view.bounds
+	}
+	
 }
 
